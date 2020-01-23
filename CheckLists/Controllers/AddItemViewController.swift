36b7db4f9879aae54f622fa6415534c84fa755a3
lpiem <lpiem@iutabgdin202-03.univ-lyon1.fr>
2020-01-23 14:49:00 +0100

    q28

--- CheckLists/Controllers/AddItemViewController.swift
+++ CheckLists/Controllers/AddItemViewController.swift
@@ -10,10 +10,15 @@ import UIKit
 
 class AddItemViewController: UITableViewController {
 
+    @IBOutlet weak var btnDone: UIBarButtonItem!
     @IBOutlet weak var tfNewItem: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func textFieldEdtingChanged(_ sender: Any) {
+        //btnDone.isEnabled = !tfNewItem.text!.isEmpty
     }
     
     @IBAction func cancel(_ sender: UIBarButtonItem) {
@@ -26,6 +31,22 @@ class AddItemViewController: UITableViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         tfNewItem.becomeFirstResponder()
+        btnDone.isEnabled = false
+    }
+    
+}
+
+//MARK: - Text field delegate
+extension AddItemViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let oldString = textField.text!
+        
+        let newString = oldString.replacingCharacters(in: Range(range, in: oldString)!, with: string)
+ 
+        btnDone.isEnabled = !newString.isEmpty
+        
+        return true
     }
 }
 
